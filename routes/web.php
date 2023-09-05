217967d3b52a6263ed8a429a5d03e9a836c0e84b
kanaokatakeshi <142466787+kanaokatakeshi@users.noreply.github.com>
2023-09-05 18:27:33 +0900

    プロフィールページの作成（一旦）

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('login'));
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/profile', [App\Http\Controllers\HomeController::class, 'edit'])->name('profile.edit');
+});
