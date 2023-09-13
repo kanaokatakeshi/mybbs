dfff0df59a78f72d2ea3775b502029f0e1a3f929
kanaokatakeshi <142466787+kanaokatakeshi@users.noreply.github.com>
2023-09-13 13:05:51 +0900

    create migrations and models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,24 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Comment extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'user_id',
-        'title',
+        'post_id',
         'content',
-        'image',
     ];
 
-    public function user ()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    public function comments()
+    public function post()
     {
-        return $this->hasMany(Comment::class);
+        return $this->belongsTo(Post::class);
     }
 }
